4bd5e6a610ef36636df2ad0059ba85c1641a0981
xiumubai <1547702880@qq.com>
2022-09-03 16:39:25 +0800

    docs(数据结构): array

--- docs/.vitepress/sidebar/dataStructure.js
+++ docs/.vitepress/sidebar/dataStructure.js
@@ -2,7 +2,7 @@
  * @Author: 朽木白
  * @Date: 2022-09-03 13:59:07
  * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-09-03 16:07:03
+ * @LastEditTime: 2022-09-03 16:37:17
  * @Description:
  */
 export default function sidebarDataStructure() {
@@ -20,28 +20,99 @@ export default function sidebarDataStructure() {
       text: '字符串',
       items: [
         {
-          text: '01-表示数值的字符串',
-          link: '/dataStructure/character-string/01-表示数值的字符串',
+          text: '表示数值的字符串',
+          link: '/dataStructure/character-string/表示数值的字符串',
         },
         {
-          text: '02-替换空格',
-          link: '/dataStructure/character-string/02-替换空格',
+          text: '替换空格',
+          link: '/dataStructure/character-string/替换空格',
         },
         {
-          text: '03-正则表达式匹配',
-          link: '/dataStructure/character-string/03-正则表达式匹配',
+          text: '正则表达式匹配',
+          link: '/dataStructure/character-string/正则表达式匹配',
         },
         {
-          text: '04-字符串的排列',
-          link: '/dataStructure/character-string/04-字符串的排列',
+          text: '字符串的排列',
+          link: '/dataStructure/character-string/字符串的排列',
         },
         {
-          text: '05-字符串翻转',
-          link: '/dataStructure/character-string/05-字符串翻转',
+          text: '字符串翻转',
+          link: '/dataStructure/character-string/字符串翻转',
         },
         {
-          text: '06-字符流中第一个不重复的字符',
-          link: '/dataStructure/character-string/06-字符流中第一个不重复的字符',
+          text: '字符流中第一个不重复的字符',
+          link: '/dataStructure/character-string/字符流中第一个不重复的字符',
+        },
+      ],
+    },
+    {
+      text: '数组',
+      items: [
+        {
+          text: '概览',
+          link: '/dataStructure/array/概览',
+        },
+        {
+          text: '把数组排成最小的数',
+          link: '/dataStructure/array/把数组排成最小的数',
+        },
+        {
+          text: '第一个只出现一次的字符',
+          link: '/dataStructure/array/第一个只出现一次的字符',
+        },
+        {
+          text: '调整数组顺序使奇数位于偶数前面',
+          link: '/dataStructure/array/调整数组顺序使奇数位于偶数前面',
+        },
+        {
+          text: '构建乘积数组',
+          link: '/dataStructure/array/构建乘积数组',
+        },
+        {
+          text: '和为S的连续正整数序列',
+          link: '/dataStructure/array/和为S的连续正整数序列',
+        },
+        {
+          text: '和为S的两个数字',
+          link: '/dataStructure/array/和为S的两个数字',
+        },
+        {
+          text: '连续子数组的最大和',
+          link: '/dataStructure/array/连续子数组的最大和',
+        },
+        {
+          text: '两数之和',
+          link: '/dataStructure/array/两数之和',
+        },
+        {
+          text: '扑克牌顺子',
+          link: '/dataStructure/array/扑克牌顺子',
+        },
+        {
+          text: '三数之和',
+          link: '/dataStructure/array/三数之和',
+        },
+
+        {
+          text: '数组中出现次数超过数组长度一半的数字',
+          link: '/dataStructure/array/数组中出现次数超过数组长度一半的数字',
+        },
+        {
+          text: '数组中的逆序对',
+          link: '/dataStructure/array/数组中的逆序对',
+        },
+
+        {
+          text: '顺时针打印矩阵',
+          link: '/dataStructure/array/顺时针打印矩阵',
+        },
+        {
+          text: '四数之和',
+          link: '/dataStructure/array/四数之和',
+        },
+        {
+          text: '在排序数组中查找数字',
+          link: '/dataStructure/array/在排序数组中查找数字',
         },
       ],
     },
