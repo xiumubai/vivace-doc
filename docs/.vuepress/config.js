f83974541145b03b7376b1cc9a3ca96561926cf2
xiumubai <1547702880@qq.com>
2022-09-03 08:47:35 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: 朽木白
+ * @Date: 2022-09-03 01:09:44
+ * @LastEditors: 1547702880@qq.com
+ * @LastEditTime: 2022-09-03 01:09:52
+ * @Description:
+ */
+module.exports = {
+  title: 'Hello VitePress',
+  description: 'Just playing around.',
+};
